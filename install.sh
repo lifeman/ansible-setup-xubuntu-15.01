fff05f10ba243596b6188c5a3d10283b9636d644
lifeman <lifeman.web@gmail.com>
2016-01-13 08:06:03 +0200

    init

--- install.sh
+++ install.sh
@@ -8,7 +8,11 @@ then
     sudo apt-get install -y -qq python python-pip git git-core openssh-server
 
     echo "\033[0;32m Installation ansible.......\033[0m"
-    sudo pip install ansible
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install ansible
+
+
 
     echo "\033[0;32m Clone de la config d'installation.\033[0m"
     sudo -u `whoami` -H git clone https://github.com/lifeman/ansible-setup-xubuntu.git $HOME/"$SETUPDIR"
